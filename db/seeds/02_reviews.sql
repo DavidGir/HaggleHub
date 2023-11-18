e0432960cc250ca3f84b2b26e4a8e7c7dbd7bff5
David Giroux <DavidGiroux149@gmail.com>
2023-11-18 17:58:24 -0500

    add reviews seeds (fake data)

---
+++ db/seeds/02_reviews.sql
@@ -0,0 +1,9 @@
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (4, 1, 5, 'Absolutely love this Funko Pop! Great addition to my collection.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (6, 2, 4, 'Really detailed and well-made, but the packaging was slightly damaged.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (7, 3, 3, 'Looks good, but not as high quality as I expected.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (8, 4, 5, 'Perfect! My favorite character and the quality is top-notch.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (4, 5, 2, 'The paint job was not great, and it arrived late.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (9, 6, 4, 'Very happy with this purchase, although it was a bit pricey.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (5, 7, 5, 'I am so impressed with the detail! A must-have for fans.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (6, 8, 3, 'Average quality, nothing too special.');
+INSERT INTO reviews (user_id, product_id, rating, review) VALUES (3, 9, 4, 'Love it! But the shipping took longer than expected.');
