c121b08a4e8350df6a852786f3a7c454dcaee04c
David Giroux <DavidGiroux149@gmail.com>
2023-11-18 17:35:15 -0500

    add SQL for users seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Ana', 'ana@email.com', "123", true);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Gerald', 'gg@email.com', "123", true);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('David', 'dd@email.com', "123", true);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Derrick', 'derrick@email.com', "123", false);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Alice', 'ai@email.com', "123", false);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Albert', 'bigal@email.com', "123", false);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Ken', 'hadouken@email.com', "123", false);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Daimon', 'daimon@email.com', "123", false);
+INSERT INTO users (username, email, password, is_Admin) VALUES ('Elizabeth', 'queen@email.com', "123", false);
+
+
