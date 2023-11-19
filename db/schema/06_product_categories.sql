42ab3ce05d3f4ebb3d90bb210291962bdf474e29
David Giroux <DavidGiroux149@gmail.com>
2023-11-18 20:23:58 -0500

    remove syntax error

--- db/schema/06_product_categories.sql
+++ db/schema/06_product_categories.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS product_categories CASCADE;
 CREATE TABLE "product_categories"(
     "id" SERIAL PRIMARY KEY NOT NULL,
     "product_id" INTEGER REFERENCES products(id) ON DELETE CASCADE,
-    "category_id" INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+    "category_id" INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
