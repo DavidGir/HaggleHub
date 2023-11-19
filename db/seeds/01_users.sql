863e6f6b01b8d14e3093cec4b96e0ba0bb6bbcd2
David Giroux <DavidGiroux149@gmail.com>
2023-11-18 20:39:59 -0500

    change syntax for seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,13 +1,13 @@
 -- Users table seeds here (Example)
 INSERT INTO users (username, email, password, is_admin)
-VALUES ('Ana', 'ana@email.com', "123", true),
-('Gerald', 'gg@email.com', "123", true),
-('David', 'dd@email.com', "123", true),
-('Derrick', 'derrick@email.com', "123", false),
-('Alice', 'ai@email.com', "123", false),
-('Albert', 'bigal@email.com', "123", false),
-('Ken', 'hadouken@email.com', "123", false),
-('Daimon', 'daimon@email.com', "123", false),
-('Elizabeth', 'queen@email.com', "123", false)
+VALUES ('Ana', 'ana@email.com', '123', true),
+('Gerald', 'gg@email.com', '123', true),
+('David', 'dd@email.com', '123', true),
+('Derrick', 'derrick@email.com', '123', false),
+('Alice', 'ai@email.com', '123', false),
+('Albert', 'bigal@email.com', '123', false),
+('Ken', 'hadouken@email.com', '123', false),
+('Daimon', 'daimon@email.com', '123', false),
+('Elizabeth', 'queen@email.com', '123', false)
 
 
