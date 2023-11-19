635c10d49a28c5e395d0c1d057b3397a57dbc78d
Ana <anmeuri@gmail.com>
2023-11-19 11:10:47 -0800

    Add new endpoints

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const usersRoutes = require('./routes/users');
 const productsRoutes = require('./routes/products');
 const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
@@ -41,9 +42,12 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
+app.use('/products/favorites', productsRoutes);
+app.use('/products/:id', productsRoutes);
 app.use('/register', registerRoutes);
-
 app.use('/login', loginRoutes);
+
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
