45371c68fc40632971b2de6a2b66ee5a501e10d2
David Giroux <DavidGiroux149@gmail.com>
2023-11-18 20:37:35 -0500

    add syntax corrections

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -6,5 +6,5 @@ CREATE TABLE "users"(
     "username" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "is_Admin" BOOLEAN NOT NULL
+    "is_admin" BOOLEAN NOT NULL
 );
