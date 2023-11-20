d529b5df2df1368c564bb93c53838a2db9b4219f
David Giroux <DavidGiroux149@gmail.com>
2023-11-20 12:43:55 -0500

    add login logic if user or password not exists

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const router  = express.Router();
+const { getUserByUsername } = require('../db/queries/users');
+
+// GET Routes:
 
 // Route to display the login form:
 router.get('/', (req, res) => {
+
+
+
   res.render('login');
 });
 
+
+
+// -----------------------------------------------------------------------------
+
+// POST routes:
+
 // Route to handle login form submission:
 router.post('/', (req, res) => {
-  res.redirect('products');
+  // Registered user just logs in with their username and password (email not required):
+  const { username, password } = req.body;
+
+  getUserByUsername(username)
+    .then(user => {
+      if (!user) {
+        // User not found:
+        res.status(403).send("Invalid login credentials.");
+      } else if (!bcrypt.compareSync(password, user.password)) {
+        // Password does not match
+        res.status(403).send("Invalid login credentials.");
+      } else {
+        // Successful login
+        // Set user information in the session cookie
+        // Storing user ID in the cookie:
+        req.session.userId = user.id;
+        // Storing user username:
+        req.session.username = user.username;
+        res.redirect('/products');
+      }
+    })
+    .catch(err => {
+      // Handle errors
+      console.log('Error during login:', err.message);
+      res.redirect('/login');
+    });
 });
+
 
 module.exports = router;
