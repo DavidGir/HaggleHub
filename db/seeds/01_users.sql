1b960dee4b76d6ae9576a77e3d9b9c74ce8df626
David Giroux <DavidGiroux149@gmail.com>
2023-11-20 13:54:42 -0500

    change db password to hashed password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,13 +1,13 @@
 -- Users table seeds here (Example)
 INSERT INTO users (username, email, password, is_admin)
-VALUES ('Ana', 'ana@email.com', '123', true),
-('Gerald', 'gg@email.com', '123', true),
-('David', 'dd@email.com', '123', true),
-('Derrick', 'derrick@email.com', '123', false),
-('Alice', 'ai@email.com', '123', false),
-('Albert', 'bigal@email.com', '123', false),
-('Ken', 'hadouken@email.com', '123', false),
-('Daimon', 'daimon@email.com', '123', false),
-('Elizabeth', 'queen@email.com', '123', false)
+VALUES ('Ana', 'ana@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', true),
+('Gerald', 'gg@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', true),
+('David', 'dd@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', true),
+('Derrick', 'derrick@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false),
+('Alice', 'ai@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false),
+('Albert', 'bigal@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false),
+('Ken', 'hadouken@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false),
+('Daimon', 'daimon@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false),
+('Elizabeth', 'queen@email.com', '$2a$10$UuzsN3nRoH5kMTicfxbpweipQR6BM2apBL9cea5YkLMeMifH6Q1W6', false)
 
 
