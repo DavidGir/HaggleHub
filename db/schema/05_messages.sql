95c6a04e087e32996d4e21c823070fb59333bb9c
David Giroux <DavidGiroux149@gmail.com>
2023-11-23 20:01:40 -0500

    modified messages data in sql seeds

--- db/schema/05_messages.sql
+++ db/schema/05_messages.sql
@@ -4,6 +4,6 @@ CREATE TABLE "messages"(
     "product_id" INTEGER REFERENCES products(id) ON DELETE CASCADE,
     "sender_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
     "receiver_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    "content" VARCHAR(255) NOT NULL,
-    "sent_date" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+    "content" VARCHAR(255),
+    "sent_date" TIMESTAMP(0) WITHOUT TIME ZONE
 );
