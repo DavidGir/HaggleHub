fbd589f559fba64efc6b1151914e6376cdcd8fdd
David Giroux <DavidGiroux149@gmail.com>
2023-11-23 20:03:13 -0500

    add messages GET routes

--- routes/messages-api.js
+++ routes/messages-api.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const database = require('../db/queries/messages');
 
-
-// Fetch all messages
 // Fetch all messages
 router.get('/', (req, res) => {
   const { senderId, receiverId } = req.query;
@@ -34,7 +32,6 @@ router.get('/:senderId/:receiverId', (req, res) => {
     .then(messages => res.json(messages))
     .catch(error => {
       console.error('Error getting messages between two users:', error);
-      res.status(500).json({ error: 'Internal server error' });
     });
 });
 
@@ -54,5 +51,36 @@ router.post('/', (req, res) => {
       res.status(500).json({ error: 'Internal server error', details: error.message });
     });
 });
+
+// Returns a list of users who have exchanged messages with the current user
+router.get('/user-list', (req, res) => {
+  // Get logged-in user's id
+  const userId = req.session.user.id;
+  database.getUsersWithMessages(userId)
+    .then(users => {
+      res.json(users);
+      console.log(users);
+    })
+    .catch(error => {
+      console.log("Error cannot fetch users with messages", error.message);
+    });
+});
+
+// Route to get messages between the logged-in user and a selected user
+router.get('/:loggedInUserId/:selectedUserId', (req, res) => {
+
+  const { loggedInUserId, selectedUserId } = req.params;
+
+  database.getConversationBetweenUsers(loggedInUserId, selectedUserId)
+    .then(messages => {
+      res.json(messages);
+    })
+    .catch(error => {
+      console.log('Error getting conversation between users:', error.message);
+    });
+});
+
+
+
 
 module.exports = router;
