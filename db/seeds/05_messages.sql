cd35b270e89a10a2632c289375cdcf88af53993d
David Giroux <DavidGiroux149@gmail.com>
2023-11-23 20:02:07 -0500

    modify messages seeds

--- db/seeds/05_messages.sql
+++ db/seeds/05_messages.sql
@@ -1,10 +1,10 @@
 INSERT INTO messages (product_id, sender_id, receiver_id, content, sent_date) VALUES
 (1, 4, 2, 'Is this item still available?', '2023-03-10 10:00:00'),
 
-(2, 2, 4, 'Yes, it is still available 😁', '2023-03-11 11:30:00'),
+(1, 2, 4, 'Yes, it is still available 😁', '2023-03-11 11:30:00'),
 
 (3, 8, 1, 'Would you accept a lower price?', '2023-03-12 15:45:00'),
 
-(4, 1, 8, 'No we do not accept lower prices. We do have similar Funko Pops that may suit the price range that you are looking for!', '2023-03-13 09:20:00'),
+-- (3, 1, 8, 'No we do not accept lower prices. We do have similar Funko Pops that may suit the price range that you are looking for!', '2023-03-13 09:20:00'),
 
 (5, 7, 3, 'Do you have more photos of the item?', '2023-03-14 16:15:00')
